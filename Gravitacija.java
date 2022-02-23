a3694f3a2686211b7f836be31118aab9a8e3b7f5
globevnik <globevnikg@gmail.com>
2022-02-23 22:54:31 +0100

    v2.0

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,23 @@
+import java.util.*;
+
 public class Gravitacija {
 
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+
+        Scanner sc = new Scanner(System.in);     
+        
+
+        double CM = 398571280000000.0;
+        double r = 6371000.0;
+
+        double v = sc.nextDouble();
+
+        double rv2 = Math.pow(r + v, 2);
+
+
+        double result = CM / rv2;
+        
+        System.out.println(result + "m/s2");
     }
 
 }
